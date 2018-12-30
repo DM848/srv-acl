86d485516871dd01327f34cb534e0e12d800bc7d
Anders Øen Fylling <anders@nordic.email>
2018-12-30 17:31:29 +0100

    adds CORS

--- routes.go
+++ routes.go
@@ -21,8 +21,18 @@ type ACLInfo struct {
 	ACLConfig  []*ACLEntry  `json:"acl_endpoints,omitempty"`
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, PUT, DELETE")
+
+	// TODO: what are the required jolie headers?
+	//(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func SetupRoutes(router *httprouter.Router, ACLState *State) {
 	router.GET("/configuration", func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		setupResponse(&w, r)
+
 		response := &JSend{
 			HTTPCode: http.StatusOK,
 		}
